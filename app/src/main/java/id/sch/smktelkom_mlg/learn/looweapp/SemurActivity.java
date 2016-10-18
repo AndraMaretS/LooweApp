31828269f29e7cc0775b7c2fac04d1e65a4fbc2b
Andra Maret <andramaret4@gmail.com>
2016-10-18 08:21:15 +0700

    Layout 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/looweapp/SemurActivity.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.learn.looweapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SemurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semur);
+
+        setTitle("Semur Jengkol");
+    }
+}
